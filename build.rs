ff810d9bd04df6ebc0b6bac66a2816b7168b6c06
ahabhgk <ahabhgk@gmail.com>
2022-05-13 11:40:13 +0800

    feat: example

--- build.rs
+++ build.rs
@@ -4,7 +4,6 @@ fn main() {
   println!("cargo:rerun-if-changed=build.rs");
   println!("cargo:rerun-if-changed=embedded_sass.proto");
   Config::new()
-    .bytes(&["."])
     .out_dir("src/pb")
     .compile_protos(&["embedded_sass.proto"], &["."])
     .unwrap();
--- examples/compile_path.rs
+++ examples/compile_path.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use sass_embedded_host_rust::{compile, Options};
+
+#[tokio::main]
+async fn main() {
+  let res = compile(
+    "/Users/bytedance/Codes/sass-embedded-host-rust/examples/simple.scss"
+      .to_string(),
+    Options::default(),
+  )
+  .await
+  .unwrap();
+  dbg!(res.loaded_urls);
+}
