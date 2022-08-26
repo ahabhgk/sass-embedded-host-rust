a697c5a6ca2454c43f19f5aa5950e61570b6862a
ahabhgk <ahabhgk@gmail.com>
2022-08-26 13:05:38 +0800

    fix: rerun build script

--- build.rs
+++ build.rs
@@ -4,15 +4,12 @@ use prost_build::Config;
 
 fn main() {
   println!("cargo:rerun-if-changed=build.rs");
-
   println!("cargo:rerun-if-changed=ext/sass/sass-embedded.proto");
   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
   Config::new()
     .out_dir(&out_dir)
     .compile_protos(&["ext/sass/sass-embedded.proto"], &["."])
     .unwrap();
-
-  println!("cargo:rerun-if-changed=rustfmt.toml");
   Command::new("rustfmt")
     .arg(out_dir.join("sass.embedded_protocol.rs"))
     .spawn()
